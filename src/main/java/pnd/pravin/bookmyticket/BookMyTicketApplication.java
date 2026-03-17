5914155531bf86cf5d9f7fbab6dd336f38e87168
Pravin Nirmal <pravin.nirmal.doss@gmail.com>
2026-03-17 11:17:27 +0530

    Hello world commit

---
+++ src/main/java/pnd/pravin/bookmyticket/BookMyTicketApplication.java
@@ -0,0 +1,13 @@
+package pnd.pravin.bookmyticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyTicketApplication.class, args);
+    }
+
+}
